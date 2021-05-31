159df8154a450d17dcb206528f5d655f66403530
Marcelino Neto <marcelino.gneto@gmail.com>
2021-05-31 15:27:42 -0300

    Usando o printStackTrace para analisar a sequencia de chamadas de erro

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,18 @@ public class Program {
 
 	public static void main(String[] args) {
 
+	method1();	
+	System.out.println("End of program");
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");		
+		method2();
+		System.out.println("***METHOD2 START***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner(System.in);
 		try { 
 			String[] vect = sc.nextLine().split(" ");
@@ -15,13 +27,16 @@ public class Program {
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
+			e.printStackTrace(); //função usada para rastrear a sequência de chamadas que gerou a exceção (erro)
+			sc.next();//usado para aguardar comando enquanto se observa a exceção
 		}
 		catch (InputMismatchException e) {
 			System.out.println("Input error");
+			e.printStackTrace();//função usada para rastrear a sequência de chamadas que gerou a exceção (erro)
+			sc.next();//usado para aguardar comando enquanto se observa a exceção
 		}
-		System.out.println("End of program");
-				
-		sc.close();
+		sc.close();	
+		System.out.println("***METHOD2 END***");
 	}
 
 }
